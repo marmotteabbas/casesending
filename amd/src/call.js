0185b54302ccbfc89fcbc01adf65a7cd287dd2a1
marmotteabbas <34753751+marmotteabbas@users.noreply.github.com>
2025-07-08 10:53:33 +0200

    dirty fix for standar platform url (wont work with subplatformpath) 1

--- amd/src/call.js
+++ amd/src/call.js
@@ -2,14 +2,14 @@ define(['jquery'], function() {
     var call = function(cas) {
         if (cas == 0) {
             currentUrl = window.location.href;
-            var form = $('<form action="https://moodle-test.grenet.fr/moodle_flo/local/casesending/view.php" method="post">' +
+            var form = $('<form action="/local/casesending/view.php" method="post">' +
             '<input type="hidden" name="url" value="'+currentUrl+'" />' +
             '</form>');
             $('body').append(form);
             $(form).submit();
         } else {
             $.ajax({
-                url: "https://moodle-test.grenet.fr/moodle_flo/local/casesending/getterbanner.php",
+                url: "/local/casesending/getterbanner.php",
                 type: 'POST',
                 data: { banumber: cas }
             }).done(function( data ) {
